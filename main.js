f498a32b930c1914c07ca5784d2e2c5f1ecbb584
Ellinelliey <hali22ow@student.ju.se>
2023-04-12 05:01:06 +0200

    h8 localstorage sm

--- main.js
+++ main.js
@@ -14,11 +14,12 @@ window.addEventListener("load", () => {
 
     e.target.reset();
 
-    displayTodos();
+    DisplayTodos();
   });
+  DisplayTodos();
 });
 
-function displayTodos() {
+function DisplayTodos() {
   const todoList = document.querySelector("#todo-list");
 
   todoList.innerHTML = "";
@@ -29,9 +30,31 @@ function displayTodos() {
 
     const label = document.createElement("label");
     const input = document.createElement("input");
+    const span = document.createElement("span");
     const content = document.createElement("div");
     const actions = document.createElement("div");
     const edit = document.createElement("button");
     const remove = document.createElement("button");
+
+    input.type = "checkbox";
+    input.checked = todo.done;
+
+    content.classList.add("todo-content");
+    actions.classlist.add("actions");
+    edit.classlist.add("edit");
+    remove.classlist.add("remove");
+
+    content.innerHTML = `<input type= "text" value= "${todo.content}" readonly>`;
+    edit.innerHTML = "Edit";
+    remove.innerHTML = "Remove";
+
+    label.appendChild(input);
+    actions.appendChild(edit);
+    actions.appendChild(remove);
+    todoItem.appendChild(label);
+    todoItem.appendChild(content);
+    todoItem.appendChild(actions);
+
+    todoItem.appendChild(todoItem);
   });
 }
