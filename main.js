0bdb325f4218eabe699b9bb8d8a12ca2d851917d
Ellinelliey <hali22ow@student.ju.se>
2023-04-12 05:56:56 +0200

    hopefully it works

--- main.js
+++ main.js
@@ -4,29 +4,29 @@ window.addEventListener("load", () => {
 
   newTodoForm.addEventListener("submit", (e) => {
     e.preventDefault();
+
     const todo = {
       content: e.target.elements.content.value,
       done: false,
     };
+
     todos.push(todo);
-
     localStorage.setItem("todos", JSON.stringify(todos));
-
     e.target.reset();
 
     DisplayTodos();
   });
+
   DisplayTodos();
 });
 
 function DisplayTodos() {
   const todoList = document.querySelector("#todo-list");
-
   todoList.innerHTML = "";
 
   todos.forEach((todo) => {
     const todoItem = document.createElement("div");
-    todoItem.classlist.add("todo-item");
+    todoItem.classList.add("todo-item");
 
     const label = document.createElement("label");
     const input = document.createElement("input");
@@ -40,21 +40,56 @@ function DisplayTodos() {
     input.checked = todo.done;
 
     content.classList.add("todo-content");
-    actions.classlist.add("actions");
-    edit.classlist.add("edit");
-    remove.classlist.add("remove");
+    actions.classList.add("actions");
+    edit.classList.add("edit");
+    remove.classList.add("remove");
 
-    content.innerHTML = `<input type= "text" value= "${todo.content}" readonly>`;
+    content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
     edit.innerHTML = "Edit";
     remove.innerHTML = "Remove";
 
     label.appendChild(input);
+    label.appendChild(span);
     actions.appendChild(edit);
     actions.appendChild(remove);
     todoItem.appendChild(label);
     todoItem.appendChild(content);
     todoItem.appendChild(actions);
+    todoList.appendChild(todoItem);
 
-    todoItem.appendChild(todoItem);
+    if (todo.done) {
+      todoItem.classList.add("done");
+    }
+
+    input.addEventListener("change", (e) => {
+      todo.done = e.target.checked;
+      localStorage.setItem("todos", JSON.stringify(todos));
+
+      if (todo.done) {
+        todoItem.classList.add("done");
+      } else {
+        todoItem.classList.remove("done");
+      }
+
+      DisplayTodos();
+    });
+
+    edit.addEventListener("click", (e) => {
+      const input = content.querySelector("input");
+      input.removeAttribute("readonly");
+      input.focus();
+      input.addEventListener("blur", (e) => {
+        input.setAttribute("readonly", true);
+        todo.content = e.target.value;
+        localStorage.setItem("todos", JSON.stringify(todos));
+        DisplayTodos();
+      });
+    });
+
+    remove.addEventListener("click", (e) => {
+      todos = todos.filter((t) => t != todo);
+      localStorage.setItem("todos", JSON.stringify(todos));
+      DisplayTodos();
+    });
   });
 }
